27981b54dfb281825731dec1d0d08f4faac5f383
ImranOneem <abouimran2@gmail.com>
2018-12-04 17:35:05 +0100

    add feature

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
              bouamama <code>src/App.js</code> and save to reload.
           </p>
+          <p>new feature</p>
           <a
             className="App-link"
             href="https://reactjs.org"
